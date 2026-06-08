2568f0264d60cb14a7bbf23fdc1cb83b167aaeb5
TaiTran205 <bidbaw@gmail.com>
2026-06-08 21:27:04 +0700

    Create inti_database.sql

---
+++ scripts/inti_database.sql
@@ -0,0 +1,16 @@
+USE master;
+GO
+-- Create Database
+Create database DataWarehouse;
+Use DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
